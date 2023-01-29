9bc5fa6b92cef22f1caf65c6be61919a967377ed
cs0041 <pamzalove20@hotmail.com>
2023-01-29 18:49:46 +0700

    add mint and Ownable

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestToken is ERC20 {
+contract TestToken is ERC20,Ownable {
     constructor(uint256 initialSupply) ERC20("TestToken", "Test")  {
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address account,uint256 amount) public onlyOwner{
+        _mint(account, amount);
     }
 }
