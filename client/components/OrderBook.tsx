eec7f22d7714963ccb294d8c38b9079d5e437883
cs0041 <pamzalove20@hotmail.com>
2023-02-04 23:48:47 +0700

    Update OrderBook.tsx

--- client/components/OrderBook.tsx
+++ client/components/OrderBook.tsx
@@ -1,7 +1,13 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ContractContext } from '../context/ContratContext'
 
 type Props = {}
+
+enum ShowOrderBookStatus {
+  BS = "BuySell",
+  S = "Sell",
+  B = "Buy"
+}
 
 function OrderBook({}: Props) {
   const {
@@ -11,11 +17,16 @@ function OrderBook({}: Props) {
     orderBookBuy,
     orderBookSell,
   } = useContext(ContractContext)
+
+  const [statusShowOrderBook, setStatusShowOrderBook] = useState<ShowOrderBookStatus>(ShowOrderBookStatus.BS)
   return (
     <div className="bg-black/20 flex flex-col flex-1 h-full">
       <header className="flex justify-between   p-5 h-[10%]">
         <div className="flex space-x-5 ">
-          <button className="space-x-1 flex flex-row hover:opacity-70">
+          <button
+            onClick={() => setStatusShowOrderBook(ShowOrderBookStatus.BS)}
+            className="space-x-1 flex flex-row hover:opacity-70"
+          >
             <div className="space-y-1">
               <div className="w-[20px] h-[20px] bg-red-500" />
               <div className="w-[20px] h-[20px] bg-green-500" />
@@ -23,12 +34,18 @@ function OrderBook({}: Props) {
             <div className="w-[20px] h-[44px] bg-gray-600 " />
           </button>
 
-          <button className="space-x-1 flex flex-row hover:opacity-70">
+          <button
+            onClick={() => setStatusShowOrderBook(ShowOrderBookStatus.B)}
+            className="space-x-1 flex flex-row hover:opacity-70"
+          >
             <div className="w-[20px] h-[44px] bg-green-500" />
             <div className="w-[20px] h-[44px] bg-gray-600 " />
           </button>
 
-          <button className="space-x-1 flex flex-row hover:opacity-70">
+          <button
+            onClick={() => setStatusShowOrderBook(ShowOrderBookStatus.S)}
+            className="space-x-1 flex flex-row hover:opacity-70"
+          >
             <div className="w-[20px] h-[44px] bg-red-500" />
             <div className="w-[20px] h-[44px] bg-gray-600 " />
           </button>
@@ -37,42 +54,46 @@ function OrderBook({}: Props) {
         <span className="text-2xl">100</span>
       </header>
 
-      <div className="grid grid-cols-3    px-5 pb-5">
+      <div className="text-sm grid grid-cols-3  gap-x-3 pr-5  pb-5 ">
         <div className="text-right">Price(USDT)</div>
         <div className="text-right">Amount(BTC)</div>
         <div className="text-right">Total</div>
       </div>
 
-      <div className="h-[44%] text-red-500 px-5 ">
-        {orderBookSell.map((item) => (
-          <div className="grid grid-cols-3 ">
-            <div className="text-right">{item.price}</div>
-            <div className="text-right"> {item.amount}</div>
-            <div className="text-right"> {item.amount * item.price}</div>
-          </div>
-        ))}
-      </div>
+      {statusShowOrderBook === ShowOrderBookStatus.B ? null : (
+        <div className="h-[44%] text-red-500   pr-5 text-base">
+          {orderBookSell.map((item) => (
+            <div className="grid grid-cols-3 gap-x-3">
+              <div className="text-right ">{item.price}</div>
+              <div className="text-right "> {item.amount - item.filled}</div>
+              <div className="text-right "> {item.amount * item.price}</div>
+            </div>
+          ))}
+        </div>
+      )}
 
-      <div className="w-full h-[4%]  " />
+      {statusShowOrderBook === ShowOrderBookStatus.BS && (
+        <div className="w-full h-[4%] text-3xl border-y-2 my-5 border-gray-500" />
+      )}
 
-      <div className="grid grid-cols-3   px-5 pb-5">
-        <div className="text-right">Price(USDT)</div>
-        <div className="text-right">Amount(BTC)</div>
-        <div className="text-right">Total</div>
-      </div>
-      <div className="h-[44%]  text-green-500 px-5 ">
-        {orderBookBuy.map((item) => (
-          <div className="grid grid-cols-3 ">
-            <div className="text-right">{item.price}</div>
-            <div className="text-right"> {item.amount}</div>
-            <div className="text-right"> {item.amount * item.price}</div>
-          </div>
-        ))}
-      </div>
+      {statusShowOrderBook === ShowOrderBookStatus.S ? null : (
+        <div className="h-[44%]  text-green-500  pr-5 text-base">
+          {orderBookBuy.map((item) => (
+            <div className="grid grid-cols-3 gap-x-3">
+              <div className="text-right">{item.price}</div>
+              <div className="text-right"> {item.amount - item.filled}</div>
+              <div className="text-right"> {item.amount * item.price}</div>
+            </div>
+          ))}
+        </div>
+      )}
 
       <button
-        className="text-black rounded-lg bg-red-500 p-2 px-8"
-        onClick={loadOrderBook}
+        className="text-black rounded-lg bg-red-500 p-2 px-8 mb-5"
+        onClick={() => {
+          loadOrderBook()
+          console.log(statusShowOrderBook)
+        }}
       >
         GetDataOrder
       </button>
