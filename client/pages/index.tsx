447b82ea40064ab79d7a63fd20b67b591c68965a
cs0041 <pamzalove20@hotmail.com>
2023-02-04 23:49:20 +0700

    remove code use components

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -18,6 +18,9 @@ import {ContractContext} from '../context/ContratContext'
 import UpdateModal from '../components/Modal'
 import Header from '../components/Header'
 import OrderBook from '../components/OrderBook'
+import PanelCommand from '../components/PanelCommand'
+import TradingviewGraph from '../components/TradingviewGraph'
+import History from '../components/History'
   
     
 interface Inputs {
@@ -30,28 +33,17 @@ interface Inputs {
 
 const Home = () => {
   const {
-    priceToken,
-    sendTxMarketOrder,
+
     balancesSpotToken0,
     balancesTradeToken0,
     balancesSpotToken1,
     balancesTradeToken1,
-    sendTxLimitOrder,
     isLoadingOrderBookByAddress,
     orderBookByAddress,
     loadOrderBookByAddress,
     sendTxCancelOrder,
   } = useContext(ContractContext)
 
-
-  const [inputBuyToken, setInputBuyToken] = useState<string>()
-  const [inputSellToken, setInputSellToken] = useState<string>()
-
-  const [inputBuyPriceTokenLimitOrder, setInputBuyPriceTokenLimitOrder] = useState<string>()
-  const [inputBuyAmountTokenLimitOrder, setInputBuyAmountTokenLimitOrder] = useState<string>()
-
-  const [inputSellPriceTokenLimitOrder, setInputSellPriceTokenLimitOrder] = useState<string>()
-  const [inputSellAmountTokenLimitOrder, setInputSellAmountTokenLimitOrder] = useState<string>()
 
   // for update modal
   const [sideBuyOrSell, setSideBuyOrSell] = useState<number>(-1)
@@ -75,248 +67,81 @@ const Home = () => {
           chainStatus={'full'}
         />
 
-        <Header/>
-        <section className='flex h-screen flex-row flex-1 '>
-          <div className='w-2/12 h-4/6 '>
-              <OrderBook/>
-       
+        <Header />
+        <section className="flex h-screen flex-col flex-1 ">
+          <div className="flex flex-row h-4/6">
+            <div className="w-2/12 min-w-fit ">
+              <OrderBook />
+            </div>
+            <div className="w-10/12 border-x-2 border-gray-600 ">
+              <div className=" h-[65%]">
+                <TradingviewGraph />
+              </div>
+              <div className=" h-[35%]">
+                <PanelCommand />
+              </div>
+            </div>
+            <div className="w-2/12">Market Trades</div>
           </div>
-          <div className='w-10/12 h-4/6 bg-green-300'>
-       
-              Grahp
+          <div className="h-2/6 border-gray-600 border-t-2 ">
+            <History />
           </div>
-          <div className='w-2/12 h-4/6 bg-orange-400'>
-              
-              Market Trades
-          </div>
-
         </section>
 
         {address && <p>My address is {address}</p>}
 
-        <span>Price</span>
-        <p>{priceToken ? priceToken : 'wait Price ...'}</p>
-
-
-
-        <h1 className="text-4xl mt-10">Market Order</h1>
-
-        <div className="mt-10 flex-row flex space-x-10 justify-center">
-          <div className="max-w-[800px] min-w-[400px] space-y-8 rounded bg-black/40 py-10 px-6 ">
-            <div className="bg-black/40 flex flex-row text-xl">
-              <span className="flex items-center pl-2 pr-5">Total</span>
-              <input
-                type="number"
-                onKeyPress={(event) => {
-                  if (!/^[0-9]*[.,]?[0-9]*$/.test(event.key)) {
-                    event.preventDefault()
-                  }
-                }}
-                onChange={(e) => {
-                  setInputBuyToken(e.target.value)
-                }}
-                className="  w-full py-2 pr-2 text-right  bg-transparent outline-none  text-white"
-              />
-              <span className="flex items-center  pr-5">USDT</span>
-            </div>
-            <button
-              onClick={() => {
-                sendTxMarketOrder(0, inputBuyToken!)
-              }}
-              className="w-full text-white rounded bg-green-500 py-3 font-semibold"
-            >
-              Buy BTC
-            </button>
-          </div>
-
-          <div className="max-w-[800px] min-w-[400px]  space-y-8 rounded bg-black/40 py-10 px-6 ">
-            <div className="bg-black/40 flex flex-row text-xl">
-              <span className="flex items-center pl-2 pr-5">Amount</span>
-              <input
-                type="number"
-                required
-                onKeyPress={(event) => {
-                  if (!/^[0-9]*[.,]?[0-9]*$/.test(event.key)) {
-                    event.preventDefault()
-                  }
-                }}
-                onChange={(e) => {
-                  setInputSellToken(e.target.value)
-                }}
-                className=" w-full py-2 pr-2 text-right   bg-transparent outline-none text-xl text-white"
-              />
-              <span className="flex items-center  pr-5">BTC</span>
-            </div>
-            <button
-              onClick={() => {
-                sendTxMarketOrder(1, inputSellToken!)
-              }}
-              className="w-full text-white rounded bg-red-500 py-3 font-semibold"
-            >
-              Sell BTC
-            </button>
-          </div>
-        </div>
-
-
-        <h1 className="text-4xl mt-10">Limit Order</h1>
-
-        <div className="mt-10 flex-row flex space-x-10 justify-center">
-          <div className="max-w-[800px] min-w-[400px] space-y-8 rounded bg-black/40 py-10 px-6 ">
-            <div className="bg-black/40 flex flex-row text-xl">
-
-              <span className="flex items-center pl-2 pr-5">Price</span>
-              <input
-                type="number"
-                onKeyPress={(event) => {
-                  if (!/^[0-9]*[.,]?[0-9]*$/.test(event.key)) {
-                    event.preventDefault()
-                  }
-                }}
-                onChange={(e) => {
-                  setInputBuyPriceTokenLimitOrder(e.target.value)
-                }}
-                className="  w-full py-2 pr-2 text-right  bg-transparent outline-none  text-white"
-              />
-              <span className="flex items-center  pr-5">USDT</span>
-            </div>
-
-            <div className="bg-black/40 flex flex-row text-xl">
-              <span className="flex items-center pl-2 pr-5">Amount</span>
-              <input
-                type="number"
-                onKeyPress={(event) => {
-                  if (!/^[0-9]*[.,]?[0-9]*$/.test(event.key)) {
-                    event.preventDefault()
-                  }
-                }}
-                onChange={(e) => {
-                  setInputBuyAmountTokenLimitOrder(e.target.value)
-                }}
-                className="  w-full py-2 pr-2 text-right  bg-transparent outline-none  text-white"
-              />
-              <span className="flex items-center  pr-5">BTC</span>
-            </div>
-            <button
-              onClick={() => {
-                 sendTxLimitOrder(0,inputBuyAmountTokenLimitOrder!,inputBuyPriceTokenLimitOrder!)
-              }}
-              className="w-full text-white rounded bg-green-500 py-3 font-semibold"
-            >
-              Buy BTC
-            </button>
-          </div>
-
-          <div className="max-w-[800px] min-w-[400px]  space-y-8 rounded bg-black/40 py-10 px-6 ">
-            <div className="bg-black/40 flex flex-row text-xl">
-              <span className="flex items-center pl-2 pr-5">Price</span>
-              <input
-                type="number"
-                required
-                onKeyPress={(event) => {
-                  if (!/^[0-9]*[.,]?[0-9]*$/.test(event.key)) {
-                    event.preventDefault()
-                  }
-                }}
-                onChange={(e) => {
-                  setInputSellPriceTokenLimitOrder(e.target.value)
-                }}
-                className=" w-full py-2 pr-2 text-right   bg-transparent outline-none text-xl text-white"
-              />
-              <span className="flex items-center  pr-5">USDT</span>
-            </div>
-            <div className="bg-black/40 flex flex-row text-xl">
-              <span className="flex items-center pl-2 pr-5">Amount</span>
-              <input
-                type="number"
-                required
-                onKeyPress={(event) => {
-                  if (!/^[0-9]*[.,]?[0-9]*$/.test(event.key)) {
-                    event.preventDefault()
-                  }
-                }}
-                onChange={(e) => {
-                  setInputSellAmountTokenLimitOrder(e.target.value)
-                }}
-                className=" w-full py-2 pr-2 text-right   bg-transparent outline-none text-xl text-white"
-              />
-              <span className="flex items-center  pr-5">BTC</span>
-            </div>
-            <button
-              onClick={() => {
-                sendTxLimitOrder(1, inputSellAmountTokenLimitOrder!,inputSellPriceTokenLimitOrder!)
-              }}
-              className="w-full text-white rounded bg-red-500 py-3 font-semibold"
-            >
-              Sell BTC
-            </button>
-          </div>
-        </div>
-
-
-
-
-
-        <div className="flex flex-row space-x-10 text-6xl">
-          <div>
-            <h1>Balances Spot</h1>
-            <p>{`Token0 : ${balancesSpotToken0}`}</p>
-            <p>{`Token1 : ${balancesSpotToken1}`}</p>
-          </div>
-          <div>
-            <h1>Balances Trade</h1>
-            <p>{`Token0 : ${balancesTradeToken0}`}</p>
-            <p>{`Token1 : ${balancesTradeToken1}`}</p>
-          </div>
-        </div>
-
         <div>
           <h1>OPEN ORDER</h1>
-          {address? (
-            isLoadingOrderBookByAddress? "Loading....... ": orderBookByAddress.map((item) => (
-                  <div className='flex flex-row mb-5 space-x-5'>
-                      <p>{`${item.BuyOrSell ==0? "Buy" : "Sell"} - price : ${item.price} - amount :  ${item.amount} - filled :  ${item.filled} ---id : ${item.id}`}</p>
-                      <button  
-                      onClick={()=>sendTxCancelOrder(item.BuyOrSell,item.id)}
-                      className=" text-white rounded bg-red-500 px-3 py-2 font-semibold">
-                        cancel order
-                      </button>
-                      <button  
-                      onClick={()=> {
+          {address
+            ? isLoadingOrderBookByAddress
+              ? 'Loading....... '
+              : orderBookByAddress.map((item) => (
+                  <div className="flex flex-row mb-5 space-x-5">
+                    <p>{`${item.BuyOrSell == 0 ? 'Buy' : 'Sell'} - price : ${
+                      item.price
+                    } - amount :  ${item.amount} - filled :  ${
+                      item.filled
+                    } ---id : ${item.id}`}</p>
+                    <button
+                      onClick={() => sendTxCancelOrder(item.BuyOrSell, item.id)}
+                      className=" text-white rounded bg-red-500 px-3 py-2 font-semibold"
+                    >
+                      cancel order
+                    </button>
+                    <button
+                      onClick={() => {
                         setIdUpdate(item.id)
                         setSideBuyOrSell(item.BuyOrSell)
                         setShowUpdateModal(true)
-                      }
-                      }
-                      className=" text-white rounded bg-orange-500 px-3 py-2 font-semibold">
-                        update order
-                      </button>
+                      }}
+                      className=" text-white rounded bg-orange-500 px-3 py-2 font-semibold"
+                    >
+                      update order
+                    </button>
                   </div>
-                
                 ))
-          )
-          
-          
-          : "PLS CONNECT WALLET"}
+            : 'PLS CONNECT WALLET'}
 
-            <button
-              onClick={() => {
-                if(address){
-                  loadOrderBookByAddress(address)
-                }else{
-                  console.log("No address")
-                }
-              }}
-              className="w-full text-white rounded bg-red-500 py-3 font-semibold"
-            >
-             Get Order By Address
-            </button>
-  
+          <button
+            onClick={() => {
+              if (address) {
+                loadOrderBookByAddress(address)
+              } else {
+                console.log('No address')
+              }
+            }}
+            className="w-full text-white rounded bg-red-500 py-3 font-semibold"
+          >
+            Get Order By Address
+          </button>
         </div>
-        {showUpdateModal && <UpdateModal
-        id={idUpdate}  
-        side={sideBuyOrSell}
-        onClose={() => setShowUpdateModal(false)}  />}
+        {showUpdateModal && (
+          <UpdateModal
+            id={idUpdate}
+            side={sideBuyOrSell}
+            onClose={() => setShowUpdateModal(false)}
+          />
+        )}
       </div>
     )
   )
