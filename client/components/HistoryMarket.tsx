0f584dde2f6c2f4fc5cce6dbcf4cc80c9c739c9b
cs0041 <pamzalove20@hotmail.com>
2023-02-06 16:37:07 +0700

    use grid

--- client/components/HistoryMarket.tsx
+++ client/components/HistoryMarket.tsx
@@ -1,6 +1,7 @@
 import { time } from 'console'
 import React, { useContext, useEffect } from 'react'
 import { ContractContext } from '../context/ContratContext'
+import { ConvertDateTime } from '../utils/DateTime'
 
 type Props = {}
 
@@ -17,17 +18,26 @@ useEffect(() => {
 }, [marketEvent])
 
   return (
-    <div className="h-full  p-3 ">
-      HistoryMarket
-      <div className="overflow-y-auto max-h-full pb-10">
+    <div className="h-full  ml-10 py-3">
+      <h1 className='text-xl text-yellow-400'>Market Trades</h1>
+      <div className="grid grid-cols-3 py-5">
+        <div>Time</div>
+        <div>Price(USDT)</div>
+        <div>Amount(BTC)</div>
+      </div>
+      <div className="overflow-y-auto max-h-full pb-10 ">
         {marketEvent.map((item, index) => (
-          <>
-            {index != 1 && (
-              <p>
-                {item.Date} - {item.Side} - {item.amount}
-              </p>
-            )}
-          </>
+          <div className=" grid grid-cols-3 text-base  py-2 ">
+            <div >{ConvertDateTime(item.Date)}</div>
+            <div
+              className={` ${
+                item.Side === 0 ? 'text-red-500' : 'text-green-500'
+              }`}
+            >
+              {item.price}
+            </div>
+            <div >{item.amount}</div>
+          </div>
         ))}
       </div>
     </div>
