e5435a9fdf8370291c4c9c24bba8312bc6df6e5d
cs0041 <pamzalove20@hotmail.com>
2023-02-15 17:07:42 +0700

    Update Header.tsx

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,40 +1,65 @@
-import React, { useContext } from 'react'
-import { ContractContext } from '../context/ContratContext'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import Link from 'next/link'
+import React from 'react'
 
 type Props = {}
 
 function Header({}: Props) {
-    const { priceToken } = useContext(ContractContext)
+    return (
+      <div className="flex flex-row   items-center justify-between  bg-black/80 px-20 py-6 space-x-10">
+        <h1 className="text-2xl font-semibold">Trust Less Protocol</h1>
 
-  return (
-    <div className="flex flex-1 items-center   bg-black/40 p-8 space-x-10">
+        <form className="flex items-center w-1/3">
+          <input
+            type="text"
+            // onKeyPress={(event) => {
+            //   if (!/^[0-9]*[.,]?[0-9]*$/.test(event.key)) {
+            //     event.preventDefault()
+            //   }
+            // }}
+            onChange={(e) => {}}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5  "
+            placeholder="Search"
+            required
+          />
 
-      <h1 className="border-r-2 pr-10 text-3xl  flex  items-center">BTC/USD</h1>
-      <div className="text-3xl flex flex-col pr-10  ">
-        <p>{priceToken ? priceToken : 'wait Price ...'}</p>
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault()
+            }}
+            className="p-2.5 ml-2 text-sm font-medium text-white bg-red-700 rounded-lg border border-red-700 hover:bg-red-800 "
+          >
+            <svg
+              className="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+        </form>
+
+        <div className='flex flex-row items-center space-x-10'>
+          <Link href="/deposit">
+            <h1 className="text-xl font-bold  hover:text-yellow-400 cursor-pointer ">Deposit</h1>
+          </Link>
+          <ConnectButton
+            label="connect web3"
+            accountStatus={'full'}
+            chainStatus={'full'}
+          />
+        </div>
       </div>
-      <div className=" flex flex-col   ">
-        <span>24h Change</span>
-        <span>-412 -2.64%</span>
-      </div>
-      <div className=" flex flex-col  ">
-        <span>24h Hight</span>
-        <span>-412 -2.64%</span>
-      </div>
-      <div className=" flex flex-col  ">
-        <span>24h Low</span>
-        <span>-412 -2.64%</span>
-      </div>
-      <div className=" flex flex-col  ">
-        <span>24h Volume(BTC)</span>
-        <span>312.32</span>
-      </div>
-      <div className=" flex flex-col  ">
-        <span>24h Volume(USDT)</span>
-        <span>871242.42</span>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Header
