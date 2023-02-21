2956129dd38cb6f7a9e5da9478f4eb7c7fd7eb9a
cs0041 <pamzalove20@hotmail.com>
2023-02-22 01:45:29 +0700

    add numberWithCommas / loadListFactoryPairAddress

--- client/pages/markets.tsx
+++ client/pages/markets.tsx
@@ -1,10 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import router from 'next/router'
 import { ContractContext } from '../context/ContratContext'
+import {numberWithCommas} from '../utils/FormatNumberComma'
 type Props = {}
 
 const markets = (props: Props) => {
-  const { listPairOrder } = useContext(ContractContext)
+  const { listPairOrder,isLoadingListFactoryPairAddress,loadListFactoryPairAddress } = useContext(ContractContext)
+  useEffect(() => {
+    loadListFactoryPairAddress()
+  
+    
+  }, [])
+  
   return (
     <div className="  flex flex-1 mt-[10vh]  items-center justify-center">
       <div className="flex flex-col space-y-5">
@@ -12,6 +19,7 @@ const markets = (props: Props) => {
         <span className="Buttonselect cursor-default py-2 max-w-fit">
           Mumbai
         </span>
+
         <div className="blue-glassmorphism">
           {/* border-radius: 16px; box-shadow: 0 4px 30px rgba(0, 0, 0, 1);
           backdrop-filter: blur(5px); -webkit-backdrop-filter: blur(5px);
@@ -19,36 +27,40 @@ const markets = (props: Props) => {
           <div
             className=" 
            border-b-[1px]   border-gray-600
-           px-5 py-2  grid grid-cols-7 space-x-10 "
+           px-5 py-2  grid grid-cols-5 space-x-10 "
           >
             <div>#</div>
             <div className="col-span-2">Name</div>
             <div>Price</div>
-            <div>Change</div>
-            <div>Volume</div>
             <div>Market Cap</div>
           </div>
-          {listPairOrder.map((item, index) => (
-            <div
-              onClick={() => {
-                router.push(
-                  `/trade/tradepair?contractaddress=${item.addressContractPair}&addresstoken0=${item.addressToken0}&addresstoken1=${item.addressToken1}`
-                )
-              }}
-              className=" 
-            rounded-t-none 
-            px-5 py-5 grid grid-cols-7 hover:bg-black/20 cursor-pointer   space-x-10  max-h-[80vh] myscroll"
-            >
-              <div>{index}</div>
-              <div className="col-span-2">
-                {item.symbolToken0}-{item.symbolToken1}
-              </div>
-              <div>{item.price}</div>
-              <div>-2.5%</div>
-              <div>$1.2M</div>
-              <div>$100M</div>
+          {isLoadingListFactoryPairAddress ? (
+            <div className="flex justify-center items-center py-3 ">
+              <div className="animate-spin rounded-full h-64 w-64 border-b-2 border-red-700" />
             </div>
-          ))}
+          ) : (
+            <>
+              {listPairOrder.map((item, index) => (
+                <div
+                  onClick={() => {
+                    router.push(
+                      `/trade/tradepair?contractaddress=${item.addressContractPair}&addresstoken0=${item.addressToken0}&addresstoken1=${item.addressToken1}`
+                    )
+                  }}
+                  className=" 
+            rounded-t-none 
+            px-5 py-5 grid grid-cols-5 hover:bg-black/20 cursor-pointer   space-x-10  max-h-[80vh] myscroll"
+                >
+                  <div>{index+1}</div>
+                  <div className="col-span-2">
+                    {item.symbolToken0}-{item.symbolToken1}
+                  </div>
+                  <div>{numberWithCommas(item.price)}</div>
+                  <div> {numberWithCommas(Number(item.price)*Number(item.totalSuplly))} {item.symbolToken1}</div>
+                </div>
+              ))}
+            </>
+          )}
         </div>
       </div>
     </div>
