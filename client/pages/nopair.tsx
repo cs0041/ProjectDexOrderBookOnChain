61d52e1d2ca8c0d8d738e6475592c970a1e68db9
cs0041 <pamzalove20@hotmail.com>
2023-02-21 21:35:10 +0700

    404 no pair

---
+++ client/pages/nopair.tsx
@@ -0,0 +1,36 @@
+import Router from 'next/router'
+import React from 'react'
+
+type Props = {}
+
+function nopair({}: Props) {
+  return (
+    <div className='flex flex-col justify-center items-center mt-20 '>
+        <h1 className='text-9xl font-semibold text-red-500'>404</h1>
+        <h1 className='text-3xl font-semibold text-red-500 mb-10'>Look like your pair token address not exist</h1>
+        <p className='text-base mb-10'>Sorry about that !!! But don't worry, you can create your own pair token</p>
+        <div className='flex flex-row space-x-10'>
+            <button 
+             onClick={() => {
+              Router.push(
+                '/'
+              )
+            }}
+            className='ButtonHover py-3'>
+                Back to homepage
+            </button>
+            <button 
+            onClick={() => {
+              Router.push(
+                '/createpair'
+              )
+            }}
+            className='ButtonHover py-3'>
+                CreatePair
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default nopair
