2b7f9f7e7b1a2d4166df7b3262eae968910dd7ed
cs0041 <pamzalove20@hotmail.com>
2023-02-24 01:03:29 +0700

    add ModalShowNew

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -20,6 +20,7 @@ import Footer from '../components/Footer'
 import { Toaster } from 'react-hot-toast'
 import { ToastContainer } from 'react-toastify'
   import 'react-toastify/dist/ReactToastify.css'
+import ModalShowNew from '../components/ModalShowNew'
 
 
 const hardhat: Chain = {
@@ -78,10 +79,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           {/* <Toaster
           position='bottom-right'
           /> */}
-          <ToastContainer
-          className= "!top-14" 
-          theme="dark"
-          />
+          <ToastContainer className="!top-14" theme="dark" />
+
+          <ModalShowNew />
         </RainbowKitProvider>
       </WagmiConfig>
     </ContractProvider>
